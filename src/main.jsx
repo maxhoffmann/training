b507c850b7f51c57f6bd4ffb3d20e7cef9d2d808
Maximilian Hoffmann <account@maximilianhoffmann.com>
2015-01-03 15:34:01 +0100

    add latest records

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 "use strict";
 var React = require('react')
 var Dashboard = require('dashboard')
-var records = require('../dist/all.json')
+var records = require('../dist/exercises.json')
 
 React.render(<Dashboard records={records} />, document.body)
